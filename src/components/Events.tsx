b7a3432aad4efe6843ff7e52cfa89fd7d0b8d556
conrongvang <42422284+conrongvang@users.noreply.github.com>
2020-10-10 20:42:15 +0700

    delete, fetch exists event (#4)

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -3,28 +3,38 @@ import { WinfunEvent } from "src/ModelDeclare";
 import { withRouter } from "react-router-dom";
 import * as moment from "moment";
 import MaterialTable from "material-table";
-import { FETCH_ALL_EVENT_API } from "../api/APIs";
+import { FETCH_ALL_EXISTS_EVENT_API, DELETE_EVENT_API } from "../api/APIs";
 import { preparedImageSrc } from "../utils";
+import Toast from "react-bootstrap/Toast";
+import Button from "react-bootstrap/Button";
 
 interface State {
   events: WinfunEvent[];
   fetching: boolean;
   error: boolean;
   message: string;
+  deleting: boolean;
+  deletedEvent?: WinfunEvent;
 }
 
 enum ActionEnum {
   FETCH_ALL_EVENTS,
   FETCH_ALL_EVENTS_SUCCESSFUL,
   FETCH_ALL_EVENTS_FAILED,
+  DELETE_EVENT,
+  DELETE_EVENT_SUCCESSFUL,
+  DELETE_EVENT_FAILED
 }
 
 type Action =
   | { type: ActionEnum.FETCH_ALL_EVENTS }
   | { type: ActionEnum.FETCH_ALL_EVENTS_SUCCESSFUL; events: WinfunEvent[]; message: string }
-  | { type: ActionEnum.FETCH_ALL_EVENTS_FAILED; message: string };
+  | { type: ActionEnum.FETCH_ALL_EVENTS_FAILED; message: string }
+  | { type: ActionEnum.DELETE_EVENT; }
+  | { type: ActionEnum.DELETE_EVENT_SUCCESSFUL; deletedEvent: WinfunEvent; message: string }
+  | { type: ActionEnum.DELETE_EVENT_FAILED; message: string };
 
-const initialState: State = { events: [], fetching: false, error: false, message: "" };
+const initialState: State = { events: [], fetching: false, error: false, message: "", deleting: false };
 
 function reducer(state: State = initialState, action: Action): State {
   switch (action.type) {
@@ -34,6 +44,12 @@ function reducer(state: State = initialState, action: Action): State {
       return { ...state, events: action.events, fetching: false, message: action.message, error: false };
     case ActionEnum.FETCH_ALL_EVENTS_FAILED:
       return { ...state, message: action.message, error: true };
+    case ActionEnum.DELETE_EVENT:
+      return { ...state, deleting: true };
+    case ActionEnum.DELETE_EVENT_SUCCESSFUL:
+      return { ...state, deleting: false, message: action.message, error: false, deletedEvent: action.deletedEvent };
+    case ActionEnum.DELETE_EVENT_FAILED:
+      return { ...state, deleting: false, message: action.message, error: true };
     default:
       return state;
   }
@@ -43,6 +59,7 @@ export default withRouter(
   React.memo((props: any) => {
     const [state, dispatch] = React.useReducer(reducer, initialState);
     const [events, setEvents] = React.useState(state.events);
+    const [showAlert, setShowAlert] = React.useState(false);
 
     const loadData = async () => {
       try {
@@ -50,7 +67,7 @@ export default withRouter(
           type: ActionEnum.FETCH_ALL_EVENTS,
         });
 
-        const response = await fetch(FETCH_ALL_EVENT_API);
+        const response = await fetch(FETCH_ALL_EXISTS_EVENT_API);
         if (response) {
           const results = await response.json();
           if (results.code != 0) {
@@ -74,6 +91,47 @@ export default withRouter(
       }
     };
 
+    const deleteEvent = async (eventId: number) => {
+      try {
+        dispatch({
+          type: ActionEnum.DELETE_EVENT,
+        });
+        const response = await fetch(DELETE_EVENT_API, {
+          method: "PUT", // or 'PUT'
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ eventId }),
+        });
+
+        if (response) {
+          const results = await response.json();
+          if (results.code != 0) {
+            dispatch({
+              type: ActionEnum.DELETE_EVENT_FAILED,
+              message: results.message.sqlMessage,
+            });
+
+            throw Error(results.message.sqlMessage);
+          }
+
+          await loadData();
+          const data = results.data as WinfunEvent;
+          dispatch({
+            type: ActionEnum.DELETE_EVENT_SUCCESSFUL,
+            deletedEvent: data,
+            message: `Delete ${data.eventName} event successful`,
+          });          
+        }
+      }
+      catch (err) {
+        console.log(err);
+      }
+      finally {
+        setShowAlert(true);
+      }
+    };
+
     React.useEffect(() => {
       loadData();
     }, []);
@@ -88,6 +146,13 @@ export default withRouter(
 
     return (
       <>
+        <Toast className="ml-2" onClose={() => setShowAlert(false)} show={showAlert} delay={3000} autohide>
+          <Toast.Header>
+            <Button className="mr-1" variant="success" style={{ width: 16, height: 16, borderRadius: 8 }} />
+            <strong className="mr-auto">Message</strong>
+          </Toast.Header>
+          <Toast.Body>{state.message}</Toast.Body>
+        </Toast>
         <MaterialTable
           title="Events"
           onRowClick={(_, rowData: WinfunEvent) => {
@@ -133,7 +198,13 @@ export default withRouter(
               icon: "delete",
               iconProps: { color: "error" },
               tooltip: "Delete User",
-              onClick: (event, rowData) => {},
+              onClick: async (event, rowData) => {
+                if (!Array.isArray(rowData) && rowData.id) {                      
+                  const res = await confirm(`Do you want delete ${rowData.eventName} event`)
+                  if(!res)  return;     
+                  deleteEvent(rowData.id);
+                }
+              }
             },
           ]}
         />
