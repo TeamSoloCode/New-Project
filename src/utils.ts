04fdddf60ed2e3812b797b5d01f36e274bc1e0bd
BruceBui5041 <brucebui5041@gmail.com>
2020-10-12 23:21:49 +0700

    Add prepareImageSrc to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,16 @@
-export function setDataToLocalStorage(key: string, value: string){
-    localStorage.setItem(key, value)
+import { IMAGE_STORAGE_API } from "./api/API";
+
+export function setDataToLocalStorage(key: string, value: string) {
+  localStorage.setItem(key, value);
 }
 
 export function getDataFromLocalStorage(key: string) {
-    return localStorage.getItem(key)
+  return localStorage.getItem(key);
 }
+
+export const prepareImageSrc = (uri: string) => {
+  if (uri.includes("https") || uri.includes("http")) {
+    return uri;
+  }
+  return IMAGE_STORAGE_API + uri;
+};
